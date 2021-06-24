43ee17c630d6724197f48be5d50d98550debdb3b
Md. Rakibul Islam <rakib1708@gmail.com>
2021-06-24 16:05:53 +0600

    Added District Model

---
+++ database/migrations/2021_06_24_093511_create_districts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->text('slug');
+            $table->string('lon',50)->nullable();
+            $table->string('lat',50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('districts');
+    }
+}
