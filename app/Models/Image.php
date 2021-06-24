e115fb0a1f4bc4963cf325dc231eb7c1a518bffa
Md. Rakibul Islam <rakib1708@gmail.com>
2021-06-24 17:42:51 +0600

    Added Image MorphModel

---
+++ app/Models/Image.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    /**
+     * Auto-apply mass assignment protection
+     *
+     * @var array
+     */
+    protected $protected = [
+        'id'
+    ];
+
+    /**
+     * The accessors to append to the model's array form
+     *
+     * @var array
+     */
+    protected $appends = [];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at', 'updated_at'
+    ];
+}
---
+++ database/migrations/2021_06_24_113128_create_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('model_type',60);
+            $table->unsignedInteger('model_id');
+            $table->text('src');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
