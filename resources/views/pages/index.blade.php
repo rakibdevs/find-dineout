dfc6878e38940f52854b0532fce2988f2ed4efbf
Md. Rakibul Islam <rakib1708@gmail.com>
2021-07-06 19:44:34 +0600

    Route Fixing

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,12 +8,12 @@
 		</div>
 	</div>
 	<div class="container mx-auto px-2 mt-10 sm:px-4 lg:px-8">
-		<offers start-point="{{ '/api/fetch/offers/' }}"></offers>
+		<offers start-point="{{ url('/api/fetch/offers/') }}"></offers>
 	</div>
 	<div class="featured-restaurents">
 		<div class="container mx-auto px-2 pt-5 sm:px-4 lg:px-8">
 			<h4 class="bar-title mb-3">Find the best dine in your location</h4>
-			<restaurent-list start-point="{{ '/api/fetch/restaurents/' }}"></restaurent-list>
+			<restaurent-list start-point="{{ url('/api/fetch/restaurents/') }}"></restaurent-list>
 		</div>
 	</div>
 
